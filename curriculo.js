4735fc6a7d9cb5585ac5baaae125c930f8e0f28f
vhsmaria <lima.fev06@gmail.com>
2024-11-21 11:44:12 -0300

    Create curriculo.js

---
+++ curriculo.js
@@ -0,0 +1,6 @@
+document.getElementById('botao1').onclick = function () {
+document.getElementById ('mensagem').innerHTML = 'lima.fev06@gmail.com';
+};
+document.getElementById('botao2').onclick = function () {
+document.getElementById ('mensagem').innerHTML = '(27) 99915-6973';
+};
